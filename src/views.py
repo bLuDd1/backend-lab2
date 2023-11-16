549fee50a44c18fd4e2b916f551d422f13d08ea0
bLuDd1 <dtavluj@gmail.com>
2023-11-16 16:51:29 +0200

    Add base code from lab1

---
+++ src/views.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+from src import app
+from datetime import datetime
+
+
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    health_status = {'status': 'OK', 'date': current_date}
+    return jsonify(health_status)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
